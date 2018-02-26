313c578421806644e01d7e43ec2748823d78cb7e
asapovk <asapovk@gmail.com>
2018-02-26 21:05:58 +0300

    initial setup

---
+++ src/components/routes/admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class AdminRoute extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Admin route</h1>
+      </div>
+    )
+  }
+}
+
+export default AdminRoute;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'react-router-redux'
+import store from './redux/store'
+import history from './history'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App/>
+      </ConnectedRouter>
+    </Provider>, document.getElementById('root'));
+
 registerServiceWorker();
